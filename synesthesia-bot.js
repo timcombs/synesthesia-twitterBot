cdd8384ac03d1f0defb3a6abcb9bcca4559ed937
Tim Combs <tim00combs@gmail.com>
2017-03-06 09:11:13 -0800

    add more comments about jpg structure to synesthesia-bot.js

--- synesthesia-bot.js
+++ synesthesia-bot.js
@@ -54,6 +54,17 @@ userStream.on('tweet', function (tweet) { console.log(tweet) });
 
 // need to grab the url of the jpg out of the tweet
 // then analyze the file with an npm pkg
+// perhaps not npm package, but def need to analyze
+// a 3 dimensional array
+/*  r = red, g = green, b = blue, a = albedo,
+    w = width = number of pixels in row, h = height = number of pixels in row
+  [
+    [[r00, g00, b00, a00], [r01, g01, b01, a01],...,[r0w, g0w, c0w, a0w]],
+    [[r10, g10, b10, a10], [r11, g11, b11, a11],...,[r1w, g1w, c1w, a1w]],
+    ...,
+    [[rh0, gh0, bh0, ah0], [rh1, gh1, bh1, ah1],..., [rhw, ghw, chw, ahw]]
+  ]
+*/
 
 //support MP4 video format with H264 format with AAC audio
 //max file size is 512K and max length is 2:20
